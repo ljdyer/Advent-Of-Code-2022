e61d9afac1ecf070ad94ece8b9c4542cbf6c921b
Laurence Dyer <ljdyer@gmail.com>
2022-12-02 21:55:15 +0000

    Update tidied_up.py

--- solutions/day2/tidied_up.py
+++ solutions/day2/tidied_up.py
@@ -12,7 +12,10 @@ SHAPE_SCORES = {'ro': 1, 'pa': 2, 'sc': 3}
 OUTCOME_SCORES = {'win': 6, 'draw': 3, 'lose': 0}
 
 shape_pairs = [(LETTER_SHAPE[opponent], LETTER_SHAPE[me]) for opponent, me in letter_pairs]
-score = sum([SHAPE_SCORES[me] + OUTCOME_SCORES[ME_OPPONENT_OUTCOME[me][opponent]] for opponent, me in shape_pairs])
+score = sum([
+    SHAPE_SCORES[me] + OUTCOME_SCORES[ME_OPPONENT_OUTCOME[me][opponent]]
+    for opponent, me in shape_pairs]
+)
 print(score)
 
 # === PART 2 ===
@@ -21,7 +24,10 @@ OPPONENT_OUTCOME_ME = {
     shape: dict(zip(['draw', 'lose', 'win'], SHAPE_ORDER[i:] + SHAPE_ORDER[:i]))
     for i, shape in enumerate(SHAPE_ORDER)
 }
-shape_outcome_pairs = [(LETTER_SHAPE[opponent], LETTER_OUTCOME[outcome]) for opponent, outcome in letter_pairs]
-score = sum([SHAPE_SCORES[OPPONENT_OUTCOME_ME[opponent][outcome]] + OUTCOME_SCORES[outcome] for opponent, outcome in shape_outcome_pairs])
-print(score)
 
+shape_outcome_pairs = [(LETTER_SHAPE[opponent], LETTER_OUTCOME[outcome]) for opponent, outcome in letter_pairs]
+score = sum([
+    SHAPE_SCORES[OPPONENT_OUTCOME_ME[opponent][outcome]] + OUTCOME_SCORES[outcome]
+    for opponent, outcome in shape_outcome_pairs
+])
+print(score)
