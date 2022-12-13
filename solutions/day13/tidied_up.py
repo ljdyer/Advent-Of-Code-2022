b329397025a30e74672ead11ca3d1327cd688312
Laurence Dyer <ljdyer@gmail.com>
2022-12-13 13:46:20 +0000

    Update tidied_up.py

--- solutions/day13/tidied_up.py
+++ solutions/day13/tidied_up.py
@@ -3,38 +3,21 @@ import json
 with open('data.txt', 'r') as f:
     data = f.read()
 
-
 # ====================
-def relation(left, right):
+def relation(l, r):
 
     # Both values are integers
-    if isinstance(left, int) and isinstance(right, int):
-        if left < right:
-            return 'lt'
-        elif left == right:
-            return 'eq'
-        else:
-            return 'gt'
+    if isinstance(l, int) and isinstance(r, int):
+        return [x[0] for x in [('lt', l<r), ('eq',l==r), ('gt', l>r)] if x[1]][0]
     # Exactly one value is an integer
-    elif isinstance(left, int):
-        left, right = [left], right.copy()
-    elif isinstance(right, int):
-        left, right = left.copy(), [right]
-    # Both values are lists
-    else:
-        left, right = left.copy(), right.copy()
-    while left and right:
-        r = relation(left.pop(0), right.pop(0))
-        if r == 'eq':
-            continue
-        else:
-            return r
-    if not left and not right:
-        return 'eq'
-    elif not left:
-        return 'lt'
-    else:
-        return 'gt'
+    l = [l] if isinstance(l, int) else l.copy()
+    r = [r] if isinstance(r, int) else r.copy()
+    while l and r:
+        rel = relation(l.pop(0), r.pop(0))
+        if rel != 'eq':
+            return rel
+    return [x[0] for x in [('eq', not l and not r), ('lt', not l), ('gt', True)] if x[1]][0]
+    
 
 # Part 1
 packet_pairs = [pp.split('\n') for pp in data.split('\n\n')]
@@ -44,13 +27,6 @@ print(sum(ordered_pairs))
 
 # Part 2
 packets = [p for pp in packet_pairs for p in pp]
-less_than_2, between_2_and_6 = (0, 0)
-for p in packets:
-    if relation(p, [[2]]) == 'lt':
-        less_than_2 += 1
-    elif relation(p, [[6]]) == 'lt':
-        between_2_and_6 += 1
-pos_2 = less_than_2 + 1
-pos_6 = pos_2 + between_2_and_6 + 1
-print(pos_2 * pos_6)
-
+less_than_6 = [p for p in packets if relation(p, [[6]]) == 'lt']
+less_than_2 = [p for p in packets if relation(p, [[2]]) == 'lt']
+print((len(less_than_2) + 1) * (len(less_than_6) + 2))
