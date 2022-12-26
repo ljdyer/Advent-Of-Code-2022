8ddfa7204486fa5c2f015af2e7d7828e3f3e2174
Laurence Dyer <ljdyer@gmail.com>
2022-12-26 10:51:44 +0000

    day25

--- solutions/day25/on_the_day/parts_1_2.py
+++ solutions/day25/on_the_day/parts_1_2.py
@@ -1,3 +1,5 @@
+import itertools
+
 def get_data(which: str = 'test'):
     if which == 'real':
         data_path = '../data.txt'
@@ -9,7 +11,7 @@ def get_data(which: str = 'test'):
         data = f.read()
     return data.splitlines()
 
-data = get_data('test')
+data = get_data('real')
 # print(data)
 
 mapping = {
@@ -32,56 +34,154 @@ def snafu_to_dec(this):
         power *= 5
     return sum
 
-# for i in data:
-#     print(snafu_to_dec(i))
-# print(sum(snafu_to_dec(dec) for dec in data))
+for i in data:
+    print(snafu_to_dec(i))
+print(sum(snafu_to_dec(dec) for dec in data))
+
+def twos(x):
+    return sum([2 * 5 ** i for i in range(x)])
+
+def ones(x):
+    return sum([5 ** i for i in range(x)])
+
+print(twos(1))
+print(twos(2))
+print(twos(3))
+
+def add_one(snafu):
+
+    snafu = [mapping[x] for x in list(snafu)]
+    if all(x == 2 for x in snafu):
+        snafu = [1] + [-2 for _ in range(len(snafu))]
+        return ''.join([rev_mapping[x] for x in snafu])
+    else:
+        for pos in range(1, len(snafu)+1):
+            if snafu[-pos] < 2:
+                snafu[-pos] += 1
+                return ''.join([rev_mapping[x] for x in snafu])
+            elif snafu[-pos] == 2:
+                # print('XXX', pos)
+                snafu[-pos] = -2
+                if snafu[-pos-1] < 2:
+                    snafu[-pos-1] += 1
+                    return ''.join([rev_mapping[x] for x in snafu])
+                else:
+                    pos += 1
+
+snafu = '2=0--0---11--01=-100'
+dec = snafu_to_dec(snafu)
+print(dec)
+remainder = 30332970236150 - dec
+print(remainder)
+# while remainder > 0:
+#     snafu = add_one(snafu)
+#     print(snafu)
+#     dec = snafu_to_dec(snafu)
+#     print(dec)
+    # print(dec, snafu)
+#     snafu = add_one(snafu)
+# print(snafu)
+# def dec_to_snafu(dec):
+
+#     digits = 1
+#     twos = 2
+#     while twos < dec:
+#         digits += 1
+#         twos = sum([2 * 5 ** i for i in range(digits)])
+#     snafu = [2] + [2 for _ in range(digits-1)]
+#     print(dec)
+#     print(snafu)
+#     current = snafu_to_dec(''.join([rev_mapping[x] for x in snafu]))
+#     remainder = dec-current
+#     pos = 1
+#     while remainder:
+#         if snafu[pos] > -2:
+#             snafu[pos] -= 1
+#             continue
+#         elif snafu[pos] == -2:
+#             snafu[pos] == 2
+#             snafu[pos-1] == 2
+
+        
+
+
+    # print(snafu_to_dec(snafu))
+    # for i in range(len(snafu)):
+    #     print(5 ** (len(snafu) - 1 - i))
+    # print()
+    # return snafu
+
+
+# print(dec_to_snafu(1))
+# print(dec_to_snafu(13))
+# print(dec_to_snafu(20))
+
+# print(snafu_to_dec('1=='))
 
 
 
-def dec_to_snafu(this):
 
-    digits = 1
-    twos = 2
-    while twos < this:
-        digits += 1
-        twos = sum([2 * 5 ** i for i in range(digits)])
-        # print(twos)
-    snafu = [0 for _ in range(digits)]
-    for i in range(1, len(snafu)+1):
-        first_position = 5 ** (len(snafu)-i)
-        next_position = first_position / 5
-        if this >= 2 * first_position:
-            snafu[i-1] += 2
-            this -= (2*first_position)
-        elif this >= first_position + (4 * next_position):
-            snafu[i-1] += 2
-            snafu[i] -= 1
-            this -= (2 * first_position)
-            this += next_position
-        elif this >= first_position + (3 * next_position):
-            snafu[i-1] += 2
-            snafu[i] -= 2
-            this -= (2 * first_position)
-            this += (2 * next_position)
-        elif this >= first_position:
-            snafu[i-1] += 1
-            this -= first_position
-        elif this >= first_position - next_position:
-            snafu[i-1] += 1
-            snafu[i] -= 1
-            this -= first_position
-            this += next_position
-        elif this >= first_position - (2 * next_position):
-            snafu[i-1] += 1
-            snafu[i] -= 2
-            this -= first_position
-            this += (2 * next_position)
-        # print(snafu, this)
-    return ''.join([rev_mapping[x] for x in snafu]), this
-    # return digits, first_position, snafu, this
+# def dec_to_snafu(this):
+
+#     digits = 1
+#     twos = 2
+#     while twos < this:
+#         digits += 1
+#         twos = sum([2 * 5 ** i for i in range(digits)])
+#         # print(twos)
+#     snafu = [0 for _ in range(digits)]
+#     for i in range(1, len(snafu)+1):
+#         first_position = 5 ** (len(snafu)-i)
+#         next_position = first_position / 5
+#         print(first_position, next_position, this)
+#         if this >= 2 * first_position:
+#             snafu[i-1] += 2
+#             this -= (2*first_position)
+#         elif this >= first_position + (4 * next_position):
+#             snafu[i-1] += 2
+#             snafu[i] -= 1
+#             this -= (2 * first_position)
+#             this += next_position
+        
+#         elif this >= first_position + (3 * next_position):
+#             snafu[i-1] += 2
+#             snafu[i] -= 2
+#             this -= (2 * first_position)
+#             this += (2 * next_position)
+#         elif this >= first_position:
+#             snafu[i-1] += 1
+#             this -= first_position
+#         elif this >= first_position - next_position:
+#             snafu[i-1] += 1
+#             snafu[i] -= 1
+#             this -= first_position
+#             this += next_position
+#         elif this >= first_position - (2 * next_position):
+#             snafu[i-1] += 1
+#             snafu[i] -= 2
+#             this -= first_position
+#             this += (2 * next_position)
+#         # print(snafu, this)
+#     return ''.join([rev_mapping[x] for x in snafu])
+#     # return digits, first_position, snafu, this
 
 
-dec_test = [1,2,3,4,5,6,7,8,9,10,15,20,2022,12345,314159265]
-for dec in dec_test:
-    print(dec, dec_to_snafu(dec))
+# dec_test = [1,2,3,4,5,6,7,8,9,10,15,20,2022,12345,314159265]
+# for dec in dec_test:
+#     print(dec, dec_to_snafu(dec))
 
+
+# for i in range(1000):
+
+#     print(dec_to_snafu(i))
+#     try:
+#         assert snafu_to_dec(dec_to_snafu(i)) == i
+#     except:
+#         print()
+#         print(i)
+#         print(snafu_to_dec(dec_to_snafu(i)))
+#         quit()
+
+# print()
+# # print(dec_to_snafu(13))
+# print(snafu_to_dec('1=='))
--- solutions/day25/tidied_up.py
+++ solutions/day25/tidied_up.py
@@ -1,95 +1,106 @@
-from itertools import cycle
-from time import time
-
-directions = ['N', 'S', 'W', 'E']
+mapping = {'=': -2, '-': -1, '0': 0, '1': 1, '2': 2}
+rev_mapping = {v: k for k, v in mapping.items()}
 
 # ====================
-def get_elves():
-    with open('data.txt', 'r') as f:
-        data = f.read().splitlines()
-    elves = set([(h,i) for h, row in enumerate(data)
-                 for i in range(len(row)) if data[h][i] == '#'])
-    return elves
+def snafu_to_dec(snafu: str) -> int:
+    """Convert from snafu to decimal"""
+
+    snafu = [mapping[x] for x in list(snafu)]
+    return sum(x * 5 ** power for power, x in enumerate(reversed(snafu)))
 
 
 # ====================
-def decide_position(elf, first_direction):
+def add_one(snafu: str) -> str:
+    """Add one to a snafu number"""
 
-    row, col = elf
-    all_surrounding = [(x, y) for x in range(row-1, row+2) for y in range (col-1, col+2) if (x, y) != (row, col)]
-    if all((i, j) not in elves for i, j in all_surrounding):
-        return (row, col)
+    snafu = [mapping[x] for x in list(snafu)]
+    if all(x == 2 for x in snafu):
+        snafu = [1] + [-2 for _ in range(len(snafu))]
+        return ''.join([rev_mapping[x] for x in snafu])
     else:
-        checks = {
-            'N': [set(((row-1, y) for y in range(col-1, col+2))), (row-1, col)],
-            'S': [set(((row+1, y) for y in range(col-1, col+2))), (row+1, col)],
-            'W': [set(((x, col-1) for x in range(row-1, row+2))), (row, col-1)],
-            'E': [set(((x, col+1) for x in range(row-1, row+2))), (row, col+1)]
-        }
-        check_order = directions[directions.index(first_direction):] + directions[:directions.index(first_direction)]
-        for direction in check_order:
-            if all(elf not in elves for elf in checks[direction][0]):
-                return checks[direction][1]
-        else:
-            return (row, col)
+        for pos in range(1, len(snafu)+1):
+            if snafu[-pos] < 2:
+                snafu[-pos] += 1
+                return ''.join([rev_mapping[x] for x in snafu])
+            elif snafu[-pos] == 2:
+                snafu[-pos] = -2
+                if snafu[-pos-1] < 2:
+                    snafu[-pos-1] += 1
+                    return ''.join([rev_mapping[x] for x in snafu])
+                else:
+                    pos += 1
 
 
 # ====================
-def count_empty():
+def add(power: int, snafu: str) -> str:
+    """Add a power of 5 to a snafu number
+    (e.g. if power=3, add 125 to snafu)"""
 
-    rows = [i for i,_ in elves]
-    cols = [j for _,j in elves]
-    max_row, min_row = (max(rows), min(rows))
-    max_col, min_col = (max(cols), min(cols))
-    total_space = (max_row + 1 - min_row) * (max_col + 1 - min_col)
-    empty_space = total_space - len(elves)
-    return empty_space
+    if power == 0:
+        return add_one(snafu)
+    else:
+        head = snafu[:-power]
+        tail = snafu[-power:]
+        head = add_one(head)
+        return head + tail
 
 
-# Part 1
-elves = get_elves()
-start_time = time()
-first_directions = cycle(directions)
-for round in range(1, 11):
-    move_positions = {}
-    stay = []
-    first_direction = next(first_directions)
-    for elf in elves:
-        proposed_position = decide_position(elf, first_direction)
-        if proposed_position not in move_positions.keys():
-            move_positions[proposed_position] = elf
+# ====================
+def next_power(num: int) -> int:
+    """Get the highest integer x such that 5**x <= num"""
+
+    power = 0
+    while 5 ** power <= num:
+        power += 1
+    return power - 1
+
+
+# ====================
+def snafu_len(dec):
+    """Get the number of characters in the snafu
+    representation of dec"""
+
+    x = 1
+    while True:
+        if sum([2 * 5 ** i for i in range(x)]) < dec:
+            x += 1
         else:
-            stay.append(elf)
-            stay.append(move_positions[proposed_position])
-            del move_positions[proposed_position]
-    elves = set(list(move_positions.keys()) + stay)
-answer = count_empty()
-end_time = time()
-time_taken = end_time - start_time
-print(f'Part 1 answer: {answer} (time taken: {time_taken:.2f}s')
+            return x
 
-# Part 2
-elves = get_elves()
-start_time = time()
-first_directions = cycle(directions)
-num_elves = len(elves)
-for round in range(1, 1_000_001):
-    prev_elves = elves
-    move_positions = {}
-    stay = []
-    first_direction = next(first_directions)
-    for elf in elves:
-        proposed_position = decide_position(elf, first_direction)
-        if proposed_position not in move_positions.keys():
-            move_positions[proposed_position] = elf
-        else:
-            stay.append(elf)
-            stay.append(move_positions[proposed_position])
-            del move_positions[proposed_position]
-    elves = set(list(move_positions.keys()) + stay)
-    if len(prev_elves.intersection(elves)) == num_elves:
-        answer = round
-        break
-end_time = time()
-time_taken = end_time - start_time
-print(f'Part 2 answer: {answer} (time taken: {time_taken:.2f}s')
+
+# ====================
+def dec_to_snafu(dec):
+    """Convert from decimal to snafu"""
+    
+    len = snafu_len(dec)
+    snafu = '1'.zfill(len)
+    current = 1
+    while current != dec:
+        remainder = dec - current
+        power = next_power(remainder)
+        snafu = (add(power, snafu))
+        current = snafu_to_dec(snafu)
+    return snafu
+
+
+# Check against examples
+with open('examples.txt', 'r') as f:
+    examples = f.read().splitlines()[1:]
+examples = [l.split() for l in examples]
+for dec, snafu in examples:
+    assert dec_to_snafu(int(dec)) == snafu
+    assert snafu_to_dec(snafu) == int(dec)
+
+# Run test
+with open('test_data.txt', 'r') as f:
+    test = f.read().splitlines()
+answer_dec = sum(snafu_to_dec(s) for s in test)
+answer_snafu = dec_to_snafu(answer_dec)
+assert answer_snafu == '2=-1=0'
+
+# Get answer
+with open('data.txt', 'r') as f:
+    test = f.read().splitlines()
+answer_dec = sum(snafu_to_dec(s) for s in test)
+answer_snafu = dec_to_snafu(answer_dec)
+print(answer_snafu)
